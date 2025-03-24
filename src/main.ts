90b6f0d8a780d9d9fabcc3f534cdf5b12610d9ab
takumakoike <takuma.151e@gmail.com>
2025-03-24 16:01:12 +0900

    送信用関数作成

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,21 @@
 // Typescriptで作成　Claspでpush
-function pushToChat(){
+function pushToChat(message: string){
+    const ss = SpreadsheetApp.getActiveSpreadsheet();
+    const sheet = ss.getActiveSheet();
+    if(sheet.getName() !== "Google chat webhook") return;
+
+    const url = PropertiesService.getScriptProperties().getProperty("WEBHOOK_URL");
+    if(!url) return;
+    const payload = JSON.stringify({"text": message})
+
+    const options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
+        method: "post",
+        contentType: "application/json",
+        payload: payload,
+    }
+    UrlFetchApp.fetch(url, options)
+}
+
+function sheetOnEdit(e){
     
 }
